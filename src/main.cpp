2a41ccf9c559e22909c2063a547f8d9cf569911f
shraddha09107 <shraddha09singh06@gmail.com>
2026-02-21 19:38:42 +0530

    Humidity and temperature display

--- src/main.cpp
+++ src/main.cpp
@@ -10,3 +10,23 @@ void setup() {
     dht.begin();
     Serial.println("DHT11 Sensor initialized");
 }
+void loop() {
+    // write your code here
+    float humidity = dht.readHumidity();
+    float temperature = dht.readTemperature();
+
+    if (isnan(humidity) || isnan(temperature)) {
+        Serial.println("Failed to read from DHT sensor!");
+        delay(2000);
+        return;
+    }
+
+    Serial.print("Humidity: ");
+    Serial.print(humidity);
+    Serial.print(" % |");
+    Serial.print("Temperature: ");
+    Serial.print(temperature);
+    Serial.println(" *C");
+
+    delay(2000); // Wait a few seconds between measurements
+}
