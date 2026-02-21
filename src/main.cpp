8727bceec4f07490ef1cf5a780ff2b0c831f1cb6
shraddha09107 <shraddha09singh06@gmail.com>
2026-02-21 19:41:48 +0530

    Adding Doxygen in the document

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,19 @@
 #include <Arduino.h>
 #include <DHT.h>
+/**
+ * @file DHT11_Monitor.ino
+ * @brief Reads temperature and humidity using DHT11 sensor and prints to Serial Monitor.
+ *
+ * @author Shraddha
+ * @date 2026-02-21
+ * @version 1.0
+ *
+ * @details
+ * This program initializes a DHT11 sensor and continuously reads humidity
+ * and temperature values every 2 seconds. If the sensor fails to respond,
+ * an error message is displayed.
+ */
+
 #define DHTTYPE DHT11
 #define DHTPIN 2
 DHT dht(DHTPIN, DHTTYPE);
